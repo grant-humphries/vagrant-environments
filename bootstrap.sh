3ef05d0b17e4fe9dec4e463d40d14230fba7ae0e
grant-humphries <grant.humphries@gmail.com>
2016-11-16 11:57:35 -0800

    added inital commands to bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+add-apt-repository ppa:ubuntugis/ppa
+apt-get update
+
+apt-get install python-dev python-pip virtualenv -y
+# apt-get install gdal-bin -y
+# apt-get install geos -y
+
+pip install zc.buildout
