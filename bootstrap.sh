6b02653ba7004b0578ff4057ddb91d055ecb7f74
grant-humphries <grant.humphries@gmail.com>
2016-11-16 22:32:14 -0800

    mostly worked out the previousing file, still need to correct things which the installation of the dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,38 @@
 #!/usr/bin/env bash
+set -e
 
-#add-apt-repository ppa:ubuntugis/ppa
-#apt-get update
+# adding the ubunutugis repo to apt provides across to additional and
+# more up-to-date gis libraries
+add-apt-repository ppa:ubuntugis/ppa
+apt-get update
 
-apt-get install python-dev python-pip virtualenv -y
-# apt-get install gdal-bin -y
-# apt-get install geos -y
+# python tools
+apt-get install -y \
+    python-dev \
+    python-pip \
+    virtualenv
 
-pip install zc.buildout
+# geospatial c-libraries
+apt-get install -y \
+    libgdal-dev \
+    libgeos-dev \
+    libproj-dev \
+    libspatialindex-dev
+
+# environment variables that will help python packages find the c/c++
+# libraries that they rely upon
+# http://gis.stackexchange.com/questions/28966/
+export C_INCLUDE_PATH='/usr/include/gdal'
+export CPLUS_INCLUDE_PATH='/usr/include/gdal'
+export PROJ_DIR='/usr/share/proj'
+
+# install python packages that are needed on system python
+pip install --upgrade pip
+pip install \
+    zc.buildout \
+    bpython
+
+# get dotfiles from git repo and deploy them
+git clone https://github.com/grant-humphries/dotfiles.git
+./dotfiles/scripts/symlink.sh
+source "${HOME}/.bashrc"
